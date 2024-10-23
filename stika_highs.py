bca721fb66ab54d8fffa4f59ff599bd8f0d07f9c
Ziyike1 <tul51428@temple.edu>
2024-10-23 14:53:01 +0800

    use matplotlib to visualize temperature csv data(partial)

--- stika_highs.py
+++ stika_highs.py
@@ -1,5 +1,7 @@
+from cProfile import label
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
 
 path = Path('weather_data/sitka_weather_07-2014.csv')
 lines = path.read_text().splitlines()
@@ -18,3 +20,16 @@ for row in reader:
     highs.append(high)
 
 print(highs)
+
+#根据最高温度绘图
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+#设置绘图的格式
+ax.set_title('Daily High Temperatures', fontsize=24)
+ax.set_xlabel('Date', fontsize=16)
+ax.set_ylabel('Temperature (F)', fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
