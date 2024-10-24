9159c10e3e91913f9eb3a7d914a464469d139383
Ziyike1 <tul51428@temple.edu>
2024-10-24 10:12:20 +0800

    complete visualize temperatures(highes and lowes) and date

--- stika_highs.py
+++ stika_highs.py
@@ -14,13 +14,15 @@ header_row = next(reader)
 # for index,column_header in enumerate(header_row):
 #     print(index, column_header)
 
-#提取最高温度
-dates, highs=[], []
+#提取最高温度,日期，和最低温度
+dates, highs, lows=[], [], []
 for row in reader:
     current_date = datetime.strptime(row[0], '%Y-%m-%d')
     high=int(row[2])
+    low = int(row[3])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 print(highs)
 print(dates)
@@ -28,10 +30,12 @@ print(dates)
 #根据最高温度绘图
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 #设置绘图的格式
-ax.set_title('Daily High Temperatures', fontsize=24)
+ax.set_title('Daily High and Low Temperatures', fontsize=24)
 ax.set_xlabel('Date', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
