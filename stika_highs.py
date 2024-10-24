6ec9717f9c4a48450b334db5a2e15fce9e2a5722
Ziyike1 <tul51428@temple.edu>
2024-10-24 10:03:47 +0800

    complete visualize temperatures and date

--- stika_highs.py
+++ stika_highs.py
@@ -1,9 +1,10 @@
-from cProfile import label
 from pathlib import Path
+from datetime import datetime
 import csv
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/sitka_weather_07-2014.csv')
+# path = Path('weather_data/sitka_weather_07-2014.csv')
+path = Path('weather_data/sitka_weather_2014.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
@@ -14,21 +15,25 @@ header_row = next(reader)
 #     print(index, column_header)
 
 #提取最高温度
-highs=[]
+dates, highs=[], []
 for row in reader:
+    current_date = datetime.strptime(row[0], '%Y-%m-%d')
     high=int(row[2])
+    dates.append(current_date)
     highs.append(high)
 
 print(highs)
+print(dates)
 
 #根据最高温度绘图
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 #设置绘图的格式
 ax.set_title('Daily High Temperatures', fontsize=24)
 ax.set_xlabel('Date', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
 ax.tick_params(labelsize=16)
 
